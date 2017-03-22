2374deb55f73c1b641a91615cf3ac647c0b3a8c0
Ra-el Peters <rpeters@thoughtworks.com>
2017-03-22 08:45:17 +0000

    Seeking feels right. Integration works

--- src/steering.js
+++ src/steering.js
@@ -5,27 +5,29 @@ const createSeeker = (position, direction, speed, target, tag) => {
         position, 
         direction, 
         speed, 
-        maxForce: 10,
+        maxSpeed: 5,
+        maxForce: 0.2,
         target,
         tag,
-        mass: 10,
+        mass: 1,
         friends: []
     };
 
     const integrate = (steeringDirection, delta) => {
         const steeringForce = steeringDirection.clone();
-        steeringForce.clampLength(0, seeker.maxForce);
+        steeringForce.clampLength(0, seeker.maxForce * delta);
         const acceleration = steeringForce.clone();
         acceleration.divideScalar(seeker.mass);
 
         const velocity = seeker.direction.clone();
-        velocity.multiplyScalar(seeker.speed);
+        velocity.multiplyScalar(seeker.speed * delta);
         velocity.add(acceleration);
-        velocity.clampLength(0, seeker.speed * delta);
+        velocity.clampLength(0, seeker.maxSpeed * delta);
 
         seeker.direction.copy
         seeker.position.add(velocity);
 
+        seeker.speed = velocity.length() / delta;
         velocity.normalize();
         seeker.direction.copy(velocity);
     };
@@ -34,15 +36,19 @@ const createSeeker = (position, direction, speed, target, tag) => {
         const desiredVelocity = seeker.target.clone();
         desiredVelocity.sub(seeker.position);
         desiredVelocity.normalize();
-        desiredVelocity.multiplyScalar(seeker.speed);
+        desiredVelocity.multiplyScalar(seeker.maxSpeed * delta);
 
         const steering = desiredVelocity.clone();
-
+        console.log("steering: " + JSON.stringify(steering));
         const velocity = seeker.direction.clone();
         velocity.multiplyScalar(seeker.speed * delta);
-        steering.add(velocity);
+        steering.sub(velocity);
 
         integrate(steering, delta);
+
+        console.log("position: " + JSON.stringify(seeker.position));
+        console.log("direction: " + JSON.stringify(seeker.direction));
+        console.log("speed: " + JSON.stringify(seeker.speed));
     };
 
     return seeker;
